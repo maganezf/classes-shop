690ad288cf083f899b4a46fafb7f2f7e19721fbb
Maganez Filho <maganezfilho@gmail.com>
2023-04-11 08:56:05 -0300

    feat(linters): eslint, prettier and editorconfig added

--- web/src/containers/Home/index.tsx
+++ web/src/containers/Home/index.tsx
@@ -1,13 +1,14 @@
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
 
 export const Homepage = () => {
-	const { user } = useUser();
+  const { user } = useUser();
 
-	return (
-		<main>
-			<h1>Hello World</h1>
-			<pre>{JSON.stringify(user, null, 2)}</pre>
-			<a href="/api/auth/logout">Logout</a>
-		</main>
-	);
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Link href="/api/auth/logout">Logout</Link>
+    </main>
+  );
 };
